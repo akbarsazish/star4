5bc6b11dc7595f05d65334a63f30f49e2f591e82
akbarzasish <sazishaliakbar@gmail.com>
2023-02-19 11:09:32 +0330

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,217 @@
+@extends('layout')
+@section('content')
+<div class="container-fluid containerDiv">
+    <div class="row">
+            <div class="col-lg-2 col-md-2 col-sm-3 sideBar">
+                <fieldset class="border rounded mt-5 sidefieldSet">
+                    <legend  class="float-none w-auto legendLabel mb-0"> تنظیمات </legend>
+                    <div class="form-check">
+                        <input class="form-check-input p-2 float-end" type="radio" name="settings" id="elseSettingsRadio">
+                        <label class="form-check-label me-4" for="assesPast">  سطح دسترسی  </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input p-2 float-end" type="radio" name="settings" id="settingAndTargetRadio">
+                        <label class="form-check-label me-4" for="assesPast"> تارگت ها و امتیازات </label>
+                    </div>
+                    
+                </fieldset>
+                </div>
+            <div class="col-sm-10 col-md-10 col-sm-12 contentDiv">
+                <div class="row contentHeader"> </div>
+                <div class="row mainContent">
+
+                </div>
+                <div class="row contentFooter"> </div>
+            </div>
+    </div>
+</div>
+
+@endsection
+
+
+baseInfoN
+baseInfoProfileN
+deleteProfileN
+editProfileN
+seeProfileN
+infoRdN
+rdSentN
+deleteSentRdN
+editSentRdN
+seeSentRdN
+rdNotSentN
+deleteRdNotSentN
+editRdNotSentN
+seeRdNotSentN
+specialSettingN
+deleteSaleLineN
+editSaleLineN
+seeSaleLineN
+baseInfoSetting
+InfoSettingAccess
+deleteSettingAccess
+editSettingAccess
+seeSettingAccess
+InfoSettingTarget
+deleteSettingTargetN
+editSettingTargetN
+seeSettingTargetN
+declareElementN
+deletedeclareElementN
+editdeclareElementN
+seedeclareElementN
+oppN
+oppTakhsisN
+oppManagerN
+deleteManagerOppN
+editManagerOppN
+seeManagerOppN
+oppHeadN
+deleteHeadOppN
+editHeadOppN
+seeHeadOppN
+deleteBazaryabOppN
+editBazaryabOppN
+seeBazaryabOppN
+oppDriverN
+deleteoppDriverServiceN
+editoppDriverServiceN
+seeoppDriverServiceN
+oppBargiriN
+deleteoppBargiriN
+editoppBargiriN
+seeoppBargiriN
+oppNazarSanjiN
+deletetodayoppNazarsanjiN
+edittodayoppNazarsanjiN
+seetodayoppNazarsanjiN
+pastoppNazarsanjiN
+deletepastoppNazarsanjiN
+editpastoppNazarsanjiN
+seepastoppNazarsanjiN
+DoneoppNazarsanjiN
+deleteDoneoppNazarsanjiN
+editDoneoppNazarsanjiN
+seeDoneoppNazarsanjiN
+OppupDownBonusN
+AddOppupDownBonusN
+deleteAddOppupDownBonusN
+editAddOppupDownBonusN
+seeAddOppupDownBonusN
+SubOppupDownBonusN
+deleteSubOppupDownBonusN
+editSubOppupDownBonusN
+seeSubOppupDownBonusN
+oppRDN
+AddedoppRDN
+deleteAddedoppRDN
+editAddedoppRDN
+seeAddedoppRDN
+NotAddedoppRDN
+deleteNotAddedoppRDN
+editNotAddedoppRDN
+seeNotAddedoppRDN
+oppCalendarN
+oppjustCalendarN
+deleteoppjustCalendarN
+editoppjustCalendarN
+seeoppjustCalendarN
+oppCustCalendarN
+deleteoppCustCalendarN
+editoppCustCalendarN
+seeoppCustCalendarN
+alarmoppN
+allalarmoppN
+deleteallalarmoppN
+editallalarmoppN
+seeallalarmoppN
+donealarmoppN
+deletedonealarmoppN
+editdonealarmoppN
+seedonealarmoppN
+NoalarmoppN
+deleteNoalarmoppN
+editNoalarmoppN
+seeNoalarmoppN
+massageOppN
+deletemassageOppN
+editmassageOppN
+seemassageOppN
+justBargiriOppN
+deletejustBargiriOppN
+editjustBargiriOppN
+seejustBargiriOppN
+reportN
+amalKardreportN
+managerreportN
+deletemanagerreportN
+editmanagerreportN
+seemanagerreportN
+deleteHeadreportN
+editHeadreportN
+seeHeadreportN
+deleteposhtibanreportN
+editposhtibanreportN
+seeposhtibanreportN
+bazaryabreportN
+deletebazaryabreportN
+editbazaryabreportN
+seebazaryabreportN
+reportDriverN
+deletereportDriverN
+editreportDriverN
+seereportDriverN
+trazEmployeeReportN
+deletetrazEmployeeReportN
+edittrazEmployeeReportN
+seetrazEmployeeReportN
+officialsN
+deleteOfficialsN
+changeOfficialsN
+seeOfficialsN
+officialsN
+deleteOfficialsN
+changeOfficialsN
+seeOfficialsN
+officialsN
+deleteOfficialsN
+changeOfficialsN
+seeOfficialsN
+officialsN
+deleteOfficialsN
+changeOfficialsN
+seeOfficialsN
+goodsReport
+salegoodsReportN
+deletesalegoodsReportN
+editsalegoodsReportN
+seesalegoodsReportN
+returnedgoodsReportN
+deletereturnedgoodsReportN
+editreturnedgoodsReportN
+seereturnedgoodsReportN
+NoExistgoodsReportN
+deleteNoExistgoodsReportN
+editNoExistgoodsReportN
+seeNoExistgoodsReportN
+nosalegoodsReportN
+deletenosalegoodsReportN
+editnosalegoodsReportN
+seenosalegoodsReportN
+returnedReportgoodsReportN
+returnedNTasReportgoodsReportN
+deletereturnedNTasReportgoodsReportN
+editreturnedNTasReportgoodsReportN
+seereturnedNTasReportgoodsReportN
+tasgoodsReprtN
+deletetasgoodsReprtN
+edittasgoodsReprtN
+seetasgoodsReprtN
+goodsbargiriReportN
+deletegoodsbargiriReportN
+editgoodsbargiriReportN
+seegoodsbargiriReportN
+
+
+
+
