f9503edf2a0c5f174567627c6c5c5b4c87503af7
akbarsazish <sazishaliakbar@gmail.com>
2023-02-20 12:15:41 +0330

    dsfsdfds

--- resources/views/salesExpert/salesExpertAction.blade.php
+++ resources/views/salesExpert/salesExpertAction.blade.php
@@ -29,11 +29,10 @@
                             <input type="text" name="" class="form-control form-control-sm" value="تا تاریخ" id="secondDateSefSaleExpert">
                         </div> 
                         <div class="col-lg-8 col-sm-12 text-start">
-                                    <button class="btn btn-primary btn-sm" type="button" onclick="openHistoryModal()"> تاریخچه عملکرد </button>
+                                <button class="btn btn-primary btn-sm" type="button" onclick="openHistoryModal()"> تاریخچه عملکرد </button>
                                 @if(hasPermission(Session::get("asn"),"trazEmployeeReportN") > 1)
                                     <button class="btn btn-primary btn-sm " id="addingEmtyazBtn"> افزودن امتیاز  <i class="fa fa-plus" aria-hidden="true"></i> </button>
                                 @endif
-                               
                                 <input type="hidden" id="adminSn" value="{{$adminId}}">
                                 <button class="btn btn-primary btn-sm" id="showEmtiyazHistoryBtn"> تاریخچه امتیاز <i class="fa fa-history" aria-hidden="true"></i> </button>
                                 <button class="btn btn-primary btn-sm" type="button" data-bs-toggle="modal" data-bs-target="#totalEmtyaz"> جمع کل امتیازات  (آذر) </button>
